8d4856494082692c670a1c2f80009028ee558b3e
0tuedon <90271995+0tuedon@users.noreply.github.com>
2025-05-12 23:44:24 +0100

    feat: using vue cli

---
+++ 07-vue-cli/new-project/src/APp.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>MY APPLICATION</h2>
+    <friend-contact></friend-contact>
+    <friend-contact></friend-contact>
+  </div>
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue';
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "tuedon",
+          name: "Tuedon Tuoyo",
+          email: "tuedon@gmail.com",
+          phone: "123 564 789",
+        },
+        {
+          id: "somto",
+          name: "Somto Nwosu",
+          email: "somto@gmail.com",
+          phone: "123 456 974",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ 07-vue-cli/new-project/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="setIsDetailsVisible">Show details</button>
+    <ul v-show="isDetailsVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isDetailsVisible: false,
+      friend: {
+        id: "tuedon",
+        name: "Tuedon Tuoyo",
+        email: "tuedon@gmail.com",
+        phone: "123 564 789",
+      },
+    };
+  },
+
+  methods: {
+    setIsDetailsVisible() {
+      this.isDetailsVisible = !this.isDetailsVisible;
+    },
+  },
+};
+</script>
